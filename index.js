124d6b4770d7effaac3e1cd207b71157fdc9f7d0
Dimitar Dzhapunov <90458809+DimOps@users.noreply.github.com>
2023-02-06 01:47:22 +0200

    Function added to delay the the action.
    Waits after confirming the language to load the main content of the page.

--- index.js
+++ index.js
@@ -8,16 +8,29 @@ async function extract() {
 
     await page.screenshot({path: 'asksForCookies.png'});
 
+    // Accepts all cookies
     const cookieNotification = await page.waitForSelector('button#onetrust-accept-btn-handler', {timeout: 3000}); 
     await cookieNotification.click();
     await cookieNotification.dispose();
 
+    await delay(3000);
     await page.screenshot({path: 'asksForLang.png'});
 
+    // Sets language option
+    const lang = await page.waitForSelector('a.modalForm__langBtn.modalForm__langBtn2.focusElem.modalFormLangBtn.ga-collect', {timeout: 3000} );
+    await lang.click();
+    await lang.dispose();
 
+    await page.screenshot({path: 'asksForEmpty.png'});
     
     
     browser.close();
 }
+
+function delay(time) {
+    return new Promise(function(resolve) { 
+        setTimeout(resolve, time)
+    });
+ }
 
 extract();
