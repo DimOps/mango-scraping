81df9a5eb2c7fca8962716d3a9e2b73fb2bcbf09
Dimitar Dzhapunov <90458809+DimOps@users.noreply.github.com>
2023-02-06 01:33:14 +0200

    Accepts cookies.
    After cookies are accepted, language options unfold successfully.

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ async function extract() {
     const page = await browser.newPage();
     await page.goto('https://shop.mango.com/gb/women/skirts-midi/midi-satin-skirt_17042020.html?c=99');
 
-    await page.screenshot({path: 'main.png'});
+    await page.screenshot({path: 'asksForCookies.png'});
 
+    const cookieNotification = await page.waitForSelector('button#onetrust-accept-btn-handler', {timeout: 3000}); 
+    await cookieNotification.click();
+    await cookieNotification.dispose();
+
+    await page.screenshot({path: 'asksForLang.png'});
+
+
+    
+    
     browser.close();
 }
 
