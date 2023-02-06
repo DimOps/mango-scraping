45bed4c56409973213d5e7738758f3274cd0e619
Dimitar Dzhapunov <90458809+DimOps@users.noreply.github.com>
2023-02-06 02:49:16 +0200

    Fixed data types.
    Displays only the value of price and all available sizes.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ async function extract() {
     
     const browser = await puppeteer.launch({
         headless: false,
-        slowMo: 400,
+        slowMo: 200,
     });
     const page = await browser.newPage();
     await page.setViewport({ width: 1600, height: 1000 });
@@ -33,27 +33,15 @@ async function extract() {
     await subscription.click();
     await subscription.dispose();
 
-    await page.screenshot({path: "cleanContentView.png", fullPage: true});
-
-    // selects size
-    await page.click("#sizesContainer > div > ul > li:nth-child(3) > button");
-
-    await page.screenshot({path: "asksForContacts.png"});
-
-    // discard out-of-stock notification form
-    const soldoutNote = await page.waitForSelector('body > div.eTS0l > div > div.cvlYZ > button', {timeout: 3000} );
-    await soldoutNote.click();
-    await soldoutNote.dispose();
-
     await page.screenshot({path: "readyToExtract.png"});
 
     const productData = await page.evaluate(() =>{
         itemName = document.querySelector('#productDesktop > main > div > div.product-actions > div.product-features-prices > div > h1').textContent;
         itemPrice = document.querySelector('#productDesktop > main > div > div.product-actions > div.product-features-prices > span > div > span.Bc91B > span > span').textContent;
         ItemColor = document.querySelector('#productDesktop > main > div > div.product-actions > div.product-colors > div.colors-info > span').textContent;
-        itemSize = document.querySelector('#size-21').textContent;
+        itemSizes = Array.from(document.querySelectorAll('#sizesContainer > div > ul li')).map(liItem => liItem.querySelector('button span').textContent);
 
-        return [itemName, itemPrice, ItemColor, itemSize]
+        return [itemName, itemPrice, ItemColor, itemSizes]
     });
 
     browser.close();
@@ -66,11 +54,11 @@ async function load (data) {
 }
 
 function transform (data) {
-    let pricing = data[1].split(' ').reverse();
+    let pricing = data[1].split(' ')[1];
 
     let obj = {
         "name":data[0],
-        "price": pricing.join(' '),
+        "price": Number(pricing),
         "color": data[2],
         "size": data[3],
     }
